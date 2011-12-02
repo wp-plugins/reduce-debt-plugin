598c427b70ad9d672edcbcabffb67247ddb08b7a
beatingdebt <beatingdebt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-02 10:51:53 +0000

    Updated plugin information.
    git-svn-id: https://plugins.svn.wordpress.org/reduce-debt-plugin/trunk@470179 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: Reduce Debt
 Description: Mobilize your BuddyPress network to reduce debt.  This plugin allows BuddyPress users to input their loan amounts in order to chart their progress, receive Encourages from friends, earn Awards, receive monthly reminders to update their loan amounts, and determine their Debt Independence Day.
 
-Version: 1.0 | By <a href="http://wordpress.org/extend/plugins/profile/beatingdebt">beatingdebt</ a> | <a href="http://wordpress.org/extend/plugins/reduce-debt-plugin/">Visit Plugin Site</a>
+Version: 1.01 | By <a href="http://wordpress.org/extend/plugins/profile/beatingdebt">beatingdebt</ a> | <a href="http://reducedebtplugin.wordpress.com/">Visit Plugin Site</a>
 */
 
 function my_plugin_init() {
